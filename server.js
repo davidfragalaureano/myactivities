a458ee8536931bc8b850bd4e8566953dce6c7e55
davidfraga <david.fraga610@hotmail.com>
2018-04-14 18:16:34 -0500

    corrections in serverjs 5

---
+++ server.js
@@ -0,0 +1,18 @@
+//Install express server
+const express       = require('express');
+const app 			= express();
+const path 			= require('path');
+const bodyParser    = require('body-parser');
+const port          = 8080;
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+//getting routes API
+var index = require('./server/routes/routes.js');
+app.use('/',index);
+
+//Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || port ,function(){
+	console.log('Server started on port: ' + port + ' ' +__dirname );
+});
