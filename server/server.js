83ebf30055afc1342ab659e4ea4c2a7f31828ee5
davidfraga <david.fraga610@hotmail.com>
2018-04-14 17:57:34 -0500

    corrections in serverjs 3

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const app 			= express();
 const path 			= require('path');
 const bodyParser    = require('body-parser');
 const port          = 8080;
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+//getting routes API
+var index = require('./routes/routes.js');
+app.use('/',index);
 
 //Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || port ,function(){
