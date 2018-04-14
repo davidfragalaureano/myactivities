6726c65257a4b33387a6215daceef04bc6dcdd00
davidfraga <david.fraga610@hotmail.com>
2018-04-14 17:02:21 -0500

    corrections in serverjs

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ const express       = require('express');
 const app 			= express();
 const path 			= require('path');
 const bodyParser    = require('body-parser');
-
+const port          = 8080;
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 
 //Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200 ,function(){
+app.listen(process.env.PORT || port ,function(){
 	console.log('Server started on port: ' + port + ' ' +__dirname );
 });
