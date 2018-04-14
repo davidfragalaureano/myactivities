704b5495d7cb56df954bd2a708ceebf319ed39b4
davidfraga <david.fraga610@hotmail.com>
2018-04-14 18:26:03 -0500

    corrections in serverjs 6

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const bodyParser    = require('body-parser');
 const port          = 8080;
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 //getting routes API
 var index = require('./server/routes/routes.js');
