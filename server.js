a83b32a4e60fa75d0ed055791259ef2eafc4ca6c
davidfraga <david.fraga610@hotmail.com>
2018-04-14 19:08:28 -0500

    corrections in serverjs 9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port          = 8080;
 app.use(express.static(path.join(__dirname + '/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist'));
+  res.sendFile(path.join(__dirname + './dist'));
 });
 
 //getting routes API
