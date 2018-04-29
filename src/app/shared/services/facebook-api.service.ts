50d4c901e61fcc6b3c8a16d233fbd88cac19c67d
davidfraga <david.fraga610@hotmail.com>
2018-04-29 16:14:32 -0500

    test onlogging event 7

--- src/app/shared/services/facebook-api.service.ts
+++ src/app/shared/services/facebook-api.service.ts
@@ -118,7 +118,7 @@ export class FacebookApiService {
    * return unread notifications
    */
    public getProfilePicture():Promise<any>{ 
-     return this.fb.api('/'+this.getuserID()+'/picture?fields=url&redirect=false'); 
+     return this.fb.api('/'+this.getuserID()+'/picture?fields=url&redirect=0'); 
    }
 
 
